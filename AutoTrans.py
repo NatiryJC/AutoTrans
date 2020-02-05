d0fabfc22a9b911bc48e350fe653a270891bea6d
NatiryJC <natiry2018@gmail.com>
2020-02-05 22:23:56 +0800

    result of first day

---
+++ AutoTrans.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+from sys import argv
+# TODO :
+#     argv    -t --translator     specify the translator
+#             -f --files          save to file(s)
+#             --without_color     without color
+#             -o --output         specify output file
+filenames = argv[1:]
+
+
+class Translator():
+    '''Translate Engines'''
+    def localTrans(seq):
+        '''local Translator Engines'''
+        # MAX ALLOWED QUERY : 500 CHARS
+        # NEED TO SPLIT PARAGRAPH INTO SENTENCES
+        from translate import Translator
+        Engine = Translator(to_lang='chinese')
+        seq = seq.split('.')
+        result = ''
+        for item in seq:
+            result += Engine.translate(item+'.')
+        return result
+
+    def YoudaoTrans(seq):
+        '''Youdao Translate API'''
+        import requests
+        url = "http://fanyi.youdao.com/translate"
+        data = {'doctype': 'json', 'type': 'AUTO', 'i': seq}
+        r = requests.get(url, params=data)
+        results = r.json()['translateResult'][0]
+        result = ''
+        for item in results:
+            result += item['tgt']
+        return result
+
+    def GoogleTrans(seq):
+        # TODO : Google Translate API
+        '''Google Translate API'''
+        pass
+
+
+def colored(seq, color_item):
+    '''print with color'''
+    tag = {
+            'red': '\033[1;31;40m',
+            'green': '\033[1;32;40m',
+            'blue': '\033[1;36;40m',
+            'fuchsia': '\033[1;35;40m',
+            'end': '\033[0m',
+          }
+    return tag[color_item]+seq+tag['end']
+
+
+def count_Num(count, Num):
+    '''print : show progress'''
+    return '('+str(count)+'/'+str(Num)+') '
+
+
+if __name__ == '__main__':
+    print(colored(":: Start Translate", 'red'))
+    for filename in filenames:
+        print(colored("->"+filename+'\n', 'fuchsia'))
+        f = open(filename, "r")
+        strings = f.read()
+        f.close()
+        strings = strings.split(".\n")
+        Num = len(strings)-1
+        count = 0
+        for seq in strings:
+            if seq == '':
+                continue
+            else:
+                seq = seq.replace('\n', ' ')+'.'
+            count += 1
+            print(colored(count_Num(count, Num)+seq, 'green'))
+            translation = Translator.YoudaoTrans(seq)
+            print(colored(count_Num(count, Num)+translation, 'blue'))
+    print(colored(":: Translate Finish", 'red'))
